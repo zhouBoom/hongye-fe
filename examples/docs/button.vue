06f1aa9bdd6dc003635623030b5fdbe2f93d94f9
zhoubin6 <zhoubin6@tal.com>
2022-09-27 11:38:58 +0800

    feat: 去除md-loader，改用vitepress

--- examples/docs/button.vue
+++ examples/docs/button.vue
@@ -1,18 +1,13 @@
 <!-- 测试markdown转html -->
 <template>
     <div class="a">v-md-editor预览组件测试：</div>
-    <v-md-preview :text="text" preview-class="vuepress-markdown-body"></v-md-preview>
+    <h-button>你好</h-button>
 </template>
 
 <script setup lang="ts">
-    import { ref, reactive, onMounted } from "vue";
-    let props = defineProps({
-        markdownTxt: String
-    }) 
-    console.log(props)
-    const text = ref(`# 序
-        开始预览组件的使用
-        * 看
-        * 写
-        * 运行`)
+    import { ref, reactive, onMounted, defineAsyncComponent } from "vue";
+    // let props = defineProps({
+    //     markdownTxt: String
+    // }) 
+    // console.log(props)
 </script>
--- examples/main.ts
+++ examples/main.ts
@@ -1,14 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue"
 import router from './routes'
-import VMdPreview from '@kangc/v-md-editor/lib/preview';
-import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
-import '@kangc/v-md-editor/lib/style/base-editor.css';
-import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
 // 引入组件库
 import HongyeUI from 'hongye-ui'; // 开发阶段
 import 'theme-chalk/src/button.scss';
 
-VMdPreview.use(vuepressTheme)
-
-createApp(App).use(router).use(HongyeUI).use(VMdPreview).mount('#app')
+createApp(App).use(router).use(HongyeUI).mount('#app')
